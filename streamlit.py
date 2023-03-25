001a4893ad6e8d6a0cea087f5babad5df0dcdbc1
speh-00 <112142283+speh-00@users.noreply.github.com>
2023-03-25 17:11:29 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -10,7 +10,7 @@ import requests
 
 def get_map_data():
     return pd.DataFrame(
-            np.random.randn(1000, 2) / [50, 50] + [1.368, 103.796],
+            np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
             columns=['lat', 'lon']
         )
 df = get_map_data()
