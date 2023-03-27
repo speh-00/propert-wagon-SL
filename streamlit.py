b1a7df678bdd9a111ab3727416feeb9bfb88ede5
speh-00 <112142283+speh-00@users.noreply.github.com>
2023-03-27 23:00:17 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -4,26 +4,35 @@ import requests
 
 st.set_page_config(layout="wide")
 
+'''
 st.title('Property Wagon - HDB resale prices')
+'''
+st.subtitle('ADD INTERACTIVE MAP SHOwING RESALE TRANSACTIONS IN PAST 12 MONTHS WITHIN 5KM')
 
-# ADD INTERACTIVE MAP?
+st.subtitle('ADD PREDICTED PRICE WITH FORECAST CHART')
+
+st.subtitle('ADD LEASE DECAY WITH AVG RENTAL DATA FOR PAST 1 YEAR')
+
+
 
 st.sidebar.header('Tell us about the HDB you are interested in')
 
-with st.form(key='params_for_api'):
+with st.sidebar.form(key='params_for_api'):
     df_flat_type = pd.DataFrame({'flat_type': ['1 ROOM', '2 ROOM', '3 ROOM', '4 ROOM', '5 ROOM', 'EXECUTIVE', 'MULTI-GENERATION']})
     df_storey_range = pd.DataFrame({'storey_range': ['01 TO 03', '04 TO 06', '07 TO 09', '10 TO 12', '13 TO 15', '16 TO 18', '19 TO 21', '22 TO 24', '25 TO 27', '28 TO 30', '31 TO 33', '34 TO 36', '37 TO 39', '40 TO 42', '43 TO 45', '46 TO 48', '49 TO 51']})
 
-    address = st.sidebar.text_input('Address', 'Enter Address')
-    flat_type = st.sidebar.selectbox('Select number of rooms', df_flat_type['flat_type'])
-    storey_range = st.sidebar.selectbox('Select level of flat', df_storey_range['storey_range'])
-    st.sidebar.form_submit_button('SUBMIT')
+    address = st.text_input('Address', 'Enter Address')
+    flat_type = st.selectbox('Select number of rooms', df_flat_type['flat_type'])
+    storey_range = st.selectbox('Select level of flat', df_storey_range['storey_range'])
+    submit_button = st.sidebar.form_submit_button('SUBMIT')
 
-    if st.form_submit_button('SUBMIT'):
-        # Make API call and display results in main section
-        response = requests.get(f"https://some-api.com?address={address}&flat_type={flat_type}&storey_range={storey_range}")
-        result = response.json()
-        st.write('result')
+#     if st.form_submit_button('SUBMIT'):
+#         # Make API call and display results in main section
+#         response = requests.get(f"https://some-api.com?address={address}&flat_type={flat_type}&storey_range={storey_range}")
+#         result = response.json()
+#         st.write('result')
+
+st.sidebar.subheader('Nearby Amenities - within 1km')        
         
         
         
