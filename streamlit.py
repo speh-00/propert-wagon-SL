9e5185385ed13b321dee05cd2103cd6ba36aa560
speh-00 <112142283+speh-00@users.noreply.github.com>
2023-03-30 21:23:26 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -4,15 +4,19 @@ import requests
 st.set_page_config(layout="wide")
 st.title('Property Wagon - HDB resale prices')
 
-const centralCoordinates = [1.290270, 103.851959];
+let taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict'; // replace with your API endpoint
+const centralCoordinates = [1.290270, 103.851959]; // starting position [lng, lat]
+
+if (window.location.href.includes('https://taxifare.lewagon.com')) {
+  taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';}
 mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
+
 const displayMap = (start, stop) => {
   const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
     center: centralCoordinates,
-    zoom: 10 // starting zoom
-  })
+    zoom: 10 // starting zoom});
 
 
 
