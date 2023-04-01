aebabd0ea3cda56a634f905068a23522fc28cb60
speh-00 <112142283+speh-00@users.noreply.github.com>
2023-04-01 15:24:03 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,48 +1,144 @@
 import streamlit as st
+import pandas as pd
+import requests
+import numpy as np
+from streamlit_folium import folium_static
+import folium
+import os
+import pandas as pd
 
-'''
-# TaxiFareModel front trying to change the whole thing
-'''
+st.set_page_config(layout="wide")
+st.title('Property Wagon - HDB resale prices')
 
-st.markdown('''
-Remember that there are several ways to output content into your web page...
 
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
-''')
 
-'''
-## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
 
-1. Let's ask for:
-- date and time
-- pickup longitude
-- pickup latitude
-- dropoff longitude
-- dropoff latitude
-- passenger count
-'''
 
-'''
-## Once we have these, let's call our API in order to retrieve a prediction
+# let taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict'; // replace with your API endpoint
+# const centralCoordinates = [1.290270, 103.851959]; // starting position [lng, lat]
 
-See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
+# if (window.location.href.includes('https://taxifare.lewagon.com')) {
+#   taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';}
+# mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
 
-🤔 How could we call our API ? Off course... The `requests` package 💡
-'''
+# const displayMap = (start, stop) => {
+#   const map = new mapboxgl.Map({
+#     container: 'map',
+#     style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+#     center: centralCoordinates,
+#     zoom: 10 // starting zoom});
 
-url = 'https://taxifare.lewagon.ai/predict'
 
-if url == 'https://taxifare.lewagon.ai/predict':
 
-    st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
 
-'''
+st.sidebar.header('Tell us about the HDB you are interested in')
 
-2. Let's build a dictionary containing the parameters for our API...
+df_flat_type = pd.DataFrame({'flat_type': ['1 ROOM', '2 ROOM', '3 ROOM', '4 ROOM', '5 ROOM', 'EXECUTIVE', 'MULTI-GENERATION']})
+df_storey_range = pd.DataFrame({'storey_range': ['01 TO 03', '04 TO 06', '07 TO 09', '10 TO 12', '13 TO 15', '16 TO 18', '19 TO 21', '22 TO 24', '25 TO 27', '28 TO 30', '31 TO 33', '34 TO 36', '37 TO 39', '40 TO 42', '43 TO 45', '46 TO 48', '49 TO 51']})
 
-3. Let's call our API using the `requests` package...
+address = st.sidebar.text_input('Address', 'Enter Address')
+flat_type = st.sidebar.selectbox('Select number of rooms', df_flat_type['flat_type'])
+storey_range = st.sidebar.selectbox('Select level of flat', df_storey_range['storey_range'])
+submit_button = st.sidebar.button('SUBMIT')
 
-4. Let's retrieve the prediction from the **JSON** returned by the API...
+st.sidebar.header('Nearby Amenities')
 
-## Finally, we can display the prediction to the user
-'''
+def geo(address):
+    response = requests.get('https://developers.onemap.sg/commonapi/search?searchVal='+address+'&returnGeom=Y&getAddrDetails=Y&pageNum=1')
+    result = response.json()
+
+    return (train, mall, hawker, school)
+
+if submit_button:
+    
+#         # Make API call and display results in main section
+#         response = requests.get(f"https://some-api.com?address={address}&flat_type={flat_type}&storey_range={storey_range}")
+#         result = response.json()
+    
+    train = 'api nearest train and distance'
+    mall = 'api nearest mall and distance'
+    hawker = 'api nearest hawker and distance'
+    school = 'api nearest school and distance'
+    supermarket = 'api nearest supermarket and distance'
+    st.sidebar.write(train)
+    st.sidebar.write(mall)
+    st.sidebar.write(hawker)
+    st.sidebar.write(school)
+                     
+st.subheader('Add a map') 
+def get_map_data():
+    return pd.DataFrame(np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4], columns=['lat', 'lon'])
+df = get_map_data()
+st.map(df)
+  
+    
+st.subheader('Try another map')     
+m = folium.Map(location=[1.290270, 103.85195], zoom_start=6)
+
+geojson_path = os.path.join("getAllPlanningarea.json")
+resale_path = os.path.join("recent_tnx.csv")
+
+for _, month in pd.read_csv(resale_path).iterrows():
+
+    folium.Marker(
+        location=[month.Latitude, month.Longitude],
+        popup=[month.resale_price, month.address, month.month],
+        icon=folium.Icon(color="red", icon="info-sign"),
+    ).add_to(m)
+    
+    
+    
+        
+        
+        
+# st.subtitle('ADD INTERACTIVE MAP SHOwING RESALE TRANSACTIONS IN PAST 12 MONTHS WITHIN 5KM')
+# @st.cache
+# def get_map_data():
+#     return pd.DataFrame(
+#             np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+#             columns=['lat', 'lon'])
+# df = get_map_data()
+# st.map(df)
+
+# st.subtitle('ADD PREDICTED PRICE WITH FORECAST CHART')
+
+# st.subtitle('ADD LEASE DECAY WITH AVG RENTAL DATA FOR PAST 1 YEAR')
+
+
+
+
+# ADD INTERACTIVE MAP?
+
+# def get_map_data():
+#     return pd.DataFrame(
+#             np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+#             columns=['lat', 'lon']
+#         )
+# df = get_map_data()
+# st.map(df)
+
+
+# '''
+
+# '''
+# ## Once we have these, let's call our API in order to retrieve a prediction
+
+
+# '''
+
+# url = 'https://taxifare.lewagon.ai/predict'
+
+# if url == 'https://taxifare.lewagon.ai/predict':
+
+#     st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
+
+# '''
+
+# 2. Let's build a dictionary containing the parameters for our API...
+
+# 3. Let's call our API using the `requests` package...
+
+# 4. Let's retrieve the prediction from the **JSON** returned by the API...
+
+# ## Finally, we can display the prediction to the user
+# '''
